044de136b2d07053db9b746465768a09d195c5be
charliehillner <charliehillner@gmail.com>
2026-06-05 14:21:52 +0200

    Initial project setup

---
+++ backend/risk-analytics-backend/src/main/java/de/charliehillner/riskanalyticsbackend/RiskAnalyticsBackendApplication.java
@@ -0,0 +1,13 @@
+package de.charliehillner.riskanalyticsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskAnalyticsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskAnalyticsBackendApplication.class, args);
+    }
+
+}
---
+++ backend/risk-analytics-backend/src/test/java/de/charliehillner/riskanalyticsbackend/RiskAnalyticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.charliehillner.riskanalyticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskAnalyticsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
